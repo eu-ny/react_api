75d9d04baeec083f26c1dafd0c97fae284038313
haeun <praise1109@naver.com>
2022-11-09 05:48:28 +0900

    20221109

--- README.md
+++ README.md
@@ -15,11 +15,17 @@ npm install axios
 ## gasp 설치
 npm install gasp
 
-## 리액트 사이트 1  - 1.퍼블리싱 / 2.데이터 / 
-## 뷰 사이트1 -  1. 퍼블리싱 / 
+## 리액트 사이트 1   -> 1.퍼블리싱 / 2.데이터 / 
+https://legendary-pastelito-6da7f0.netlify.app/   
+
+## 뷰 사이트1       ->  1. 퍼블리싱 /  2. 데이터 /
+https://vueclass5.web.app/   
 
 ## 리액트 사이트 2
+
 ## 뷰 사이트 2
 
-## 리액트 API 사이트 - 1. 로우터 / 2. API 불러오기(데이터) / 3. TOP10 슬라이드 구현(스와이프)
-## 뷰 API사이트 - 1. 로우터 / 2.
+## 리액트 API 사이트 -> 1. 로우터 / 2. API 불러오기(데이터) / 3. TOP10 슬라이드 구현(스와이프)
+
+## 뷰 API사이트     -> 1. 로우터 / 2.
+https://vue-api-01.web.app/   
--- src/components/include/MovieList.jsx
+++ src/components/include/MovieList.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // 08. 밑에서 가져온 데이터 컴포넌트 만들기.
-function MovieListInfo(props) {
+const MovieListInfo = ({rank, id, poster_path, title}) => {
     return (
         <li>
-            <a href={`https://www.themoviedb.org/movie/${props.list.id}`}>
+            <a href={`https://www.themoviedb.org/movie/${id}`}>
                 <div className="imgBox">
-                    <span className="rank">{props.rank + 1}</span>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500/${props.list.poster_path}`}
-                        alt={props.list.title}
-                    />
+                    <span className="rank">{rank + 1}</span>
+                    <div className="movieListimgBox">
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                            alt={title}
+                        />
+                    </div>
                 </div>
                 <em>
-                    <span className="title">{props.list.title}</span>
+                    <span className="title">{title}</span>
                 </em>
             </a>
         </li>
@@ -21,17 +29,40 @@ function MovieListInfo(props) {
 }
 
 // 06. 매개변수에 담긴 값을 가져옴(props)
-const MovieList = (props) => {
+const MovieList = ({lists}) => {
     return (
         <div className="movie__pop">
             <div className="container">
                 <div className="movieList__inner">
                     {/* 07. 매겨변수로 가져온 데이터 값 알맞게 뿌려주기 */}
-                    <ul>
+                    {/* <ul className="movieRank">
                         {props.lists.map((lists, index) => (
                             <MovieListInfo key={index} rank={index} list={lists}/>
                         ))}
-                    </ul>
+                    </ul> */}
+                    <Swiper
+                        slidesPerView={3}
+                        autoplay={{
+                            delay: 3000,
+                            disableOnInteraction: false,
+                        }}
+                        navigation={true}
+                        pagination={{ clickable: true }}
+                        modules={[Navigation, Autoplay]}
+                        className="mySwiper"
+                    >
+                            {lists.map((list, index) => (
+                            <SwiperSlide className="movieRank">
+                                <MovieListInfo
+                                    key={index}
+                                    rank={index}
+                                    id={list.id}
+                                    poster_path={list.poster_path}
+                                    title={list.title}
+                                />
+                            </SwiperSlide>
+                            ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
