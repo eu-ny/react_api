360626d212a3f4baaa2a0201dcc43f544aef929c
haeun <praise1109@naver.com>
2022-11-15 18:19:55 +0900

    20221115

--- src/components/include/AboutCont.jsx
+++ src/components/include/AboutCont.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+// const AboutContItem = () => {
+//     return(
+
+//     )
+// }
+
 const AboutCont = () => {
-    return <section className="cont__about">cont__about</section>;
+    return (
+        <section className="cont__about">
+            <div className="container">
+                <div className="about__inner">
+                    <div className="aboutBox">
+                        <a href="/">
+                            <figure>
+                                <img src="about." alt="" />
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 };
 
 export default AboutCont;
